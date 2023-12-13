d613b41d7ebf0084f783d9a9a8f20d49a86048c2
t29mato <tomoya.matou@gmail.com>
2023-12-13 15:57:29 +0900

    refactor: read test json from json file

--- streamlit_pdf_viewer/__init__.py
+++ streamlit_pdf_viewer/__init__.py
@@ -4,6 +4,7 @@ from pathlib import Path
 from typing import Union
 
 import streamlit.components.v1 as components
+import json
 
 _RELEASE = True
 
@@ -34,25 +35,11 @@ def pdf_viewer(input: Union[str, Path, bytes], width="100%", height="700", key=N
     return component_value
 
 
-# viewer = pdf_viewer("resources/test.pdf", height="700", width="700")
-
 if not _RELEASE:
     with open("resources/test.pdf", 'rb') as fo:
         binary = fo.read()
 
-    viewer = pdf_viewer(binary, height="700", width="500", annotations=[
-        {
-            "page": 1,
-            "x": 220,
-            "y": 155,
-            "height": 22,
-            "width": 65,
-            "color": "red",
-        },{
-            "page": 2,
-            "x": 198,
-            "y": 280,
-            "height": 7,
-            "width": 30,
-            "color": "orange",
-        }])
+    with open("resources/annotations.json", 'rb') as fo:
+        annotations = json.loads(fo.read())
+
+    viewer = pdf_viewer(binary, height="700", width="800", annotations=annotations)
