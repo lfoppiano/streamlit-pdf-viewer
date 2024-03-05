a988ef2be594d1e9ef971e28d26249f8e89beaf3
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2024-03-05 19:39:17 +0900

    extend tests to cover all combination of parameters (width, height, rendering)

--- tests/streamlit_apps/example_iframe_height.py
+++ tests/streamlit_apps/example_iframe_height.py
@@ -3,8 +3,8 @@ import os
 import streamlit as st
 
 from streamlit_pdf_viewer import pdf_viewer
-from tests.test_template_args import ROOT_DIRECTORY
+from tests import ROOT_DIRECTORY
 
-st.subheader("Test PDF Viewer using legacy iframe")
+st.subheader("Test PDF Viewer using legacy iframe with specified height")
 
 pdf_viewer(os.path.join(ROOT_DIRECTORY, "resources/test.pdf"), rendering='legacy_iframe', height=500)
