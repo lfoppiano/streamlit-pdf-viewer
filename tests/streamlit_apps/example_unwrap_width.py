cdbab7a2e4680b21d8186059810e59285c11a6f9
Luca Foppiano <Foppiano.Luca@nims.go.jp>
2024-03-05 19:55:18 +0900

    fix typo

--- tests/streamlit_apps/example_unwrap_width.py
+++ tests/streamlit_apps/example_unwrap_width.py
@@ -5,6 +5,6 @@ from tests import ROOT_DIRECTORY
 
 from streamlit_pdf_viewer import pdf_viewer
 
-st.subheader("Test PDF Viewer with arguments with specified width")
+st.subheader("Test PDF Viewer with specified width")
 
 pdf_viewer(os.path.join(ROOT_DIRECTORY, "resources/test.pdf"), width=400)
