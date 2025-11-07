55eaab7f34a0518eb894bc6bb14ef136b4a1143a
Luca Foppiano <luca@foppiano.org>
2025-11-07 06:33:51 +0000

    improve build

--- streamlit_pdf_viewer/frontend/babel.config.js
+++ streamlit_pdf_viewer/frontend/babel.config.js
@@ -2,4 +2,10 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset'
   ],
+  // Ignore large PDF.js files from Babel processing to avoid deoptimization warnings
+  ignore: [
+    /node_modules\/pdfjs-dist\/build\/pdf\.worker\.mjs$/,
+    /node_modules\/pdfjs-dist\/build\/pdf\.mjs$/,
+    /node_modules\/pdfjs-dist\/build\/pdf\.sandbox\.mjs$/
+  ]
 };
